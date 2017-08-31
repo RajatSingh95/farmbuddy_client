5c6bc6fb1990ba6af4c1e18a62b1fb1e81cd759b
RajatSingh95 <rajat.s15@iiits.in>
2017-08-30 23:14:20 -0700

    init-farm

---
+++ farmindex/views.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+
+# Create your views here.
