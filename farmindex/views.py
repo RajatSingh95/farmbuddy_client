9123002a9123decff8d0fcef2d1918cf769c890f
RajatSingh95 <rajat.s15@iiits.in>
2017-09-05 06:58:52 -0700

    index page added

--- farmindex/views.py
+++ farmindex/views.py
@@ -8,6 +8,9 @@ from django.http import HttpResponse
 def index(request):
 	return render(request, 'farmindex/index.html')
 
+def fetch(request):
+	return render(request, 'farmindex/index_pre.html')
+
 
 
 
