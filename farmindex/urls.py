12d32ad6746670d4e0e43acb02aefd27bbac61d4
RajatSingh95 <rajat.s15@iiits.in>
2017-09-13 14:59:40 -0700

    Update urls.py

--- farmindex/urls.py
+++ farmindex/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^datafetch/', views.fetch, name='fetch'),
+    url(r'^landlord/', views.landlord, name='landlord'),
+    
     
 ]
