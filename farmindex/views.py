7884da70a49e9b80df8cf8c2628a395a853efb67
RajatSingh95 <rajat.s15@iiits.in>
2017-10-11 14:21:31 -0700

    hello

--- farmindex/views.py
+++ farmindex/views.py
@@ -5,21 +5,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+# It renders the landing Page of the website.
 def index(request):
 	return render(request, 'farmindex/index.html')
+
 
 def fetch(request):
 	return render(request, 'farmindex/index_pre.html')
 
+# this view is for farm Page with Google Maps
 def landlord(request):
 	return render(request, 'farmindex/landlord.html')
 
+# This view is for all the houses on the map with their details.
 def household(request):
 	return render(request, 'farmindex/household.html')
 
+# This view is for all the wells on the map with their details.
 def wells(request):
 	return render(request, 'farmindex/wells.html')
 
+# This renders the main Page after logging in to the portal.
 def mainpage(request):
 	return render(request, 'farmindex/mainpage.html')
 
